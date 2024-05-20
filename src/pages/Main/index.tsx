a4213e86e87634ac5d47d1450a2e30e337988560
Emil Murahas <murakhas@list.ru>
2024-05-20 22:49:44 +0300

    feat: add main loader

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,14 @@
+import TelegramLoaderIcon from '../../assets/icons/telegram-loader.gif';
+import { AppLink } from '../../components/AppLink';
+import cls from './Main.module.scss';
+
 export const MainPage = () => {
-	return <div>Main</div>;
+	return (
+		<div className={cls.Main}>
+			<div>Main page</div>
+			<h2 className={cls.title}>На странице ведутся работы!</h2>
+			<AppLink to={'/chat'}>Просьба перейти на рабочий экран 👇</AppLink>
+			<img src={TelegramLoaderIcon} />
+		</div>
+	);
 };
