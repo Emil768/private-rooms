3e6a7bc23946531ad74fded28f09880b39951936
Emil Murahas <murakhas@list.ru>
2024-06-24 08:52:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,4 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	server: {
-		port: 3000,
-		host: true,
-		open: true,
-	},
 });
